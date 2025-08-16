8cf0a6c7eee6ddc883f3071e3d3350ea642a3fc3
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-08-16 20:29:57 +0330

    update

--- app/podcast/[id]/page.tsx
+++ app/podcast/[id]/page.tsx
@@ -7,14 +7,14 @@ import AudioPlayer from '@/components/AudioPlayer';
 
 
 export async function generateStaticParams() {
-    const podcastIds = await getAllPodcastIds();
-    return podcastIds.map((id: string) => ({id}));
+  const podcastIds = await getAllPodcastIds();
+  return podcastIds.map((id: string) => ({ id }));
 }
 
 export async function generateMetadata(props: any) {
   const { id } = await Promise.resolve(props.params);
   const podcast = await getPodcast(id);
-  
+
   if (!podcast) {
     notFound();
   }
@@ -26,29 +26,29 @@ export async function generateMetadata(props: any) {
 }
 
 export default async function PodcastId(porps: any) {
-    const { id } = await Promise.resolve(porps.params)
-    const podcast = await getPodcast(id);
+  const { id } = await Promise.resolve(porps.params)
+  const podcast = await getPodcast(id);
   return (
     <main className="flex-1">
-        <section className="container py-12 px-32 md:py-24 lg:py-32">
-          <Image
-            src={podcast?.coverImage || defualtCover}
-            alt={podcast?.title || "podcast cover"}
-            width={350}
-            height={225}
-            className="w-full h-60 object-cover rounded-3xl"
-            />
-            <div className="p-4">
-                <h3 className="truncate text-gray-600 text-xl font-semibold mb-2">{podcast?.title}</h3>
-                <p className="truncate text-gray-600">{podcast?.description}</p>
-            </div>
-            <div className='mt-4'>
-              <AudioPlayer exercise={podcast?.audioUrl ? { audio_url: podcast.audioUrl } : null} />
-            </div>
-        </section>
-        <section className="container py-12 md:py-24 lg:py-32">
+      <section className="container py-12 px-32 md:py-24 lg:py-32">
+        <Image
+          src={podcast?.coverImage || defualtCover}
+          alt={podcast?.title || "podcast cover"}
+          width={350}
+          height={225}
+          className="w-full h-60 object-cover rounded-3xl"
+        />
+        <div className="p-4">
+          <h3 className="truncate text-gray-600 text-xl font-semibold mb-2">{podcast?.title}</h3>
+          <p className="truncate text-gray-600">{podcast?.description}</p>
+        </div>
+        <div className='mt-4'>
+          <AudioPlayer exercise={podcast?.audioUrl ? { audio_url: podcast.audioUrl } : null} />
+        </div>
+      </section>
+      <section className="container py-12 md:py-24 lg:py-32">
 
-        </section>    
+      </section>
     </main>
   )
 }
